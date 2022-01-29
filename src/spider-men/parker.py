98dae5197622af4d746ed41be5c0567b5e88ca86
Jama Noor <jamawadi@gmail.com>
2022-01-29 17:22:26 +0100

    parker lines and flask working

---
+++ src/spider-men/parker.py
@@ -0,0 +1,15 @@
+import json
+import random
+from flask import Flask
+
+with open('parker-1.json', 'r') as json_file:
+	parker_lines = json.load(json_file)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return random.choice(parker_lines)
+
+if __name__ == '__main__':
+    app.run(debug=True)
