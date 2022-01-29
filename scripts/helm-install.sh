78e5bcfce7cce516bc1b37128666618b0bb136dd
Jama Noor <jamawadi@gmail.com>
2022-01-29 18:49:50 +0100

    It goes on and on and on

--- scripts/helm-install.sh
+++ scripts/helm-install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+bash ${BASH_SOURCE%/*}/../.secrets/create-secret.sh
 helm upgrade --install spider-men ${BASH_SOURCE%/*}/../Charts/spider-men
--- scripts/k3d-create-clusters.sh
+++ scripts/k3d-create-clusters.sh
@@ -8,11 +8,11 @@ cluster_count_spidermen=3;
 echo "Creating spider-men cluster"
 
 k3d cluster delete spider-men-cluster || true
-k3d cluster create spider-men-cluster --servers $cluster_count_spidermen
+k3d cluster create spider-men-cluster --agents $cluster_count_spidermen
 
 for (( i=0; i<=$cluster_count_spidermen-1; i++ ))
 do
-    kubectl label node k3d-spider-men-cluster-server-$i universe=spider-men --overwrite
+    kubectl label node k3d-spider-men-cluster-agent-$i universe=spider-men --overwrite
 done
 
 
