1caad77c65d07e8e95d3d31f28693c736aa4f6e6
Jama Noor <jamawadi@gmail.com>
2022-01-30 10:59:31 +0100

    Oh, Dr. Connors! How are you?

--- scripts/k3d-create-clusters.sh
+++ scripts/k3d-create-clusters.sh
@@ -11,9 +11,9 @@ k3d cluster delete spider-men-cluster || true
 k3d cluster create --config ${BASH_SOURCE%/*}/../k3d/k3d.spider-men-cluster.yaml
 # k3d cluster create spider-men-cluster --api-port 6550 -p "8080:80@loadbalancer" --agents $cluster_count_spidermen
 
-# for (( i=0; i<=$cluster_count_spidermen-1; i++ ))
-# do
-#     kubectl label node k3d-spider-men-cluster-agent-$i universe=spider-men --overwrite
-# done
+for (( i=0; i<=$cluster_count_spidermen-1; i++ ))
+do
+    kubectl label node k3d-spider-men-cluster-agent-$i universe=spider-men --overwrite
+done
 
 
