7a685e49620c4c0d0eecd17f9f49523822d0b60d
Jama Noor <jamawadi@gmail.com>
2022-01-30 10:51:07 +0100

    You want forgiveness? Get religion.

--- scripts/k3d-create-clusters.sh
+++ scripts/k3d-create-clusters.sh
@@ -8,12 +8,12 @@ cluster_count_spidermen=3;
 echo "Creating spider-men cluster"
 
 k3d cluster delete spider-men-cluster || true
-k3d cluster create spider-men-cluster --config ${BASH_SOURCE%/*}/../k3d/k3d.yaml
+k3d cluster create --config ${BASH_SOURCE%/*}/../k3d/k3d.spider-men-cluster.yaml
 # k3d cluster create spider-men-cluster --api-port 6550 -p "8080:80@loadbalancer" --agents $cluster_count_spidermen
 
-for (( i=0; i<=$cluster_count_spidermen-1; i++ ))
-do
-    kubectl label node k3d-spider-men-cluster-agent-$i universe=spider-men --overwrite
-done
+# for (( i=0; i<=$cluster_count_spidermen-1; i++ ))
+# do
+#     kubectl label node k3d-spider-men-cluster-agent-$i universe=spider-men --overwrite
+# done
 
 
